e53387b5e22aa17c5ebc2425c38e05c8aba07404
SirPablo17 <pablo.rosario2019@gmail.com>
2026-08-20 23:33:24 -0300

    feat: adicionando ResourceMessageException.resx

--- src/Backend/MySchedule.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
+++ src/Backend/MySchedule.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -1,6 +1,7 @@
 using System.Data;
 using FluentValidation;
 using MySchedule.Communication.Requests;
+using MySchedule.Exception;
 
 namespace MySchedule.Application.UseCases.User.Register;
 
@@ -8,9 +9,9 @@ public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUse
 {
     public RegisterUserAccountValidator()
     {
-        RuleFor(user => user.Name).NotEmpty().WithMessage("O nome nao pode ser vazio");
-        RuleFor(user => user.Email).NotEmpty().WithMessage("O email nao pode ser vazio");
-        RuleFor(user => user.Password).NotEmpty().WithMessage("A senha nao pode ser vazia");
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.VALIDATION_NAME_REQUIRED);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessageException.VALIDATION_EMAIL_REQUIRED);
+        RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceMessageException.VALIDATION_PASSWORD_REQUIRED);
         When(user => string.IsNullOrWhiteSpace(user.Email) == false, () =>
         {
             RuleFor(user => user.Email).EmailAddress().WithMessage("O email deve ser valido");
