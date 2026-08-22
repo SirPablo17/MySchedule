831dd4f811a25845e780256225048e6ac25ca7b4
SirPablo17 <pablo.rosario2019@gmail.com>
2026-08-21 21:57:49 -0300

    feat: criando entidade User.cs

---
+++ src/Backend/MySchedule.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+namespace MySchedule.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
